98b1968a7ee20f09358cf9e21dff69b333332ca5
cenyq <cenyq@dragonpass.com.cn>
2021-08-05 10:19:12 +0800

    netty相关

---
+++ src/main/java/cn/donald/entity/Test3.java
@@ -0,0 +1,13 @@
+package cn.donald.entity;
+
+/**
+ * @Author: DonaldCen
+ * @Date: 2021/8/2 20:28
+ * @Desc:
+ */
+public class Test3 {
+
+    public static void main(String[] args) {
+        System.out.println(13 & 16);
+    }
+}
