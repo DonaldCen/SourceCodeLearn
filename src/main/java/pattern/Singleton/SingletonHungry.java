0ab34aec66eeb5f37cde7be9e631932c5f16f87b
cenyq <cenyq@dragonpass.com.cn>
2021-08-30 15:21:47 +0800

    设计模式-单例模式-饿汉模式

---
+++ src/main/java/pattern/Singleton/SingletonHungry.java
@@ -0,0 +1,18 @@
+package pattern.Singleton;
+
+/**
+ * @Author: DonaldCen
+ * @Date: 2021/8/30 14:24
+ * @Desc: 单例设计模式-饿汉方式
+ */
+public class SingletonHungry {
+
+    private static SingletonHungry instance = new SingletonHungry();
+
+    private SingletonHungry(){}
+
+    public static SingletonHungry getInstance(){
+        return instance;
+    }
+
+}
--- src/main/java/pattern/Singleton/SingletonLazy.java
+++ src/main/java/pattern/Singleton/SingletonLazy.java
@@ -1,4 +1,4 @@
-package pattern;
+package pattern.Singleton;
 
 /**
  * @Author: DonaldCen
