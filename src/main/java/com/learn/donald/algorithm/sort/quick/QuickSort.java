6b5535196e96a02242ab6f369793da34eda439f2
cenyq <cenyq@dragonpass.com.cn>
2021-09-22 11:20:46 +0800

    快速排序

--- src/main/java/com/learn/donald/algorithm/sort/quick/QuickSort.java
+++ src/main/java/com/learn/donald/algorithm/sort/quick/QuickSort.java
@@ -6,4 +6,20 @@ package com.learn.donald.algorithm.sort.quick;
  * @Desc: 快速排序
  */
 public class QuickSort {
+
+    /**
+     * 思想
+     * 找出一个中心数，分为左右两部分，
+     * 左边比这个数大的都移动到右边，右边比这个数小的都移动到左边
+     * 然后左右两部分分别按照这个规律进行递归，直到左右部分长度都为1
+     */
+    public static void sort(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        int mid = (left + right) / 2;
+        int midValue = arr[mid];
+        while (left < right){
+
+        }
+    }
 }
