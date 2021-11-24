5f0be349b96b45ea8f0470950e592fa638e92738
cenyq <cenyq@dragonpass.com.cn>
2021-11-24 11:52:10 +0800

    位图代码

---
+++ src/main/java/com/learn/donald/algorithm/bitmap/BitMapTest.java
@@ -0,0 +1,15 @@
+package com.learn.donald.algorithm.bitmap;
+
+/**
+ * @Author: DonaldCen
+ * @Date: 2021/11/23 16:09
+ * @Desc:
+ */
+public class BitMapTest {
+    public static void main(String[] args) {
+        int a = 86 + (5/8) | (1<<(5%8));
+        int a1 = 9%-9;
+        System.out.println(a1);
+        System.out.println(a);
+    }
+}
